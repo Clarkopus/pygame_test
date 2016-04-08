58d98acfd5c47f183766e4b2eaf0adcc3e13d69f
Guy Clark <guy@guy-MS-7816>
2016-04-08 03:59:38 +0100

    added text_handler.py to handle all game text. Removed old text logic

--- pygame_test.py
+++ pygame_test.py
@@ -2,6 +2,7 @@ import pygame
 import time
 import random
 import entity
+import text_handler
 
 pygame.init()
 
@@ -13,34 +14,37 @@ gameDisplay = pygame.display.set_mode((display_width,display_height))
 #set the display caption
 pygame.display.set_caption('Dodge shit get hit')
 
+text_handler = text_handler.textHandler(display_width,display_height, gameDisplay)
+race_car = entity.player(pygame.image.load('racecar.png'), 73, 82 , display_width*0.45, display_height*0.8)
+obstacle = entity.squareObject(random.randrange(0,display_width),-600,100,100,20, (0,0,0))
+obstacle2 = entity.squareObject(random.randrange(0,display_width),-600,100,100,17, (0,0,0))
+obstacle3 = entity.squareObject(random.randrange(0,display_width),-600,100,100,18, (0,0,0))
 black = (0,0,0)
 white = (255,255,255)
 #create a pygame clock to keep track of time
 clock = pygame.time.Clock()
 
-race_car = entity.player(pygame.image.load('racecar.png'), 73, 82 , display_width*0.45, display_height*0.8)
-#used to place where the car is going to be
-obstacle = entity.squareObject(random.randrange(0,display_width),-600,100,100,20, (0,0,0))
-obstacle2 = entity.squareObject(random.randrange(0,display_width),-600,100,100,17, (0,0,0))
-obstacle3 = entity.squareObject(random.randrange(0,display_width),-600,100,100,18, (0,0,0))
 
 def text_objects(text, font):
+	
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
 def message_display(text):
+	
     largeText = pygame.font.Font('freesansbold.ttf',115)
     TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = ((display_width/2),(display_height/2))
     gameDisplay.blit(TextSurf, TextRect)
     pygame.display.update()
-    time.sleep(2)   
+    time.sleep(2)
+       
  #custom function to create messages on the screen   
 def display_message(text_to_display):
 	message_display(text_to_display)
 
 def crash():
-    message_display('You Crashed')
+    text_handler.message_display('You Crashed')
     obstacle.objecty -=display_height
     obstacle.objectx = random.randrange(0,(display_width - obstacle.objectw))
     obstacle2.objecty -=display_height
@@ -107,7 +111,7 @@ def game_loop():
 #used to display the welcome message
 gameDisplay.fill(white)
 pygame.display.update()
-display_message("Welcome!")
+text_handler.display_message("Welcome!")
 #Activate the game loop
 game_loop()
 #When the game loop ends close the game
